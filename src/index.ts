78a46489e1c40a8f7628e0d4af6774df67239d71
RubenDguez <argenis.dominguez@hotmail.com>
2026-02-17 20:22:48 -0500

    Fix TypeScript Node.js types configuration

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-console.log("Playwright Cucumber Installer");
+import { execSync } from 'child_process';
+
+function isGitDirectory(): boolean {
+    try {
+        execSync('git rev-parse --git-dir', { stdio: 'pipe' });
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+function main() {
+    
+}
+
+main();
