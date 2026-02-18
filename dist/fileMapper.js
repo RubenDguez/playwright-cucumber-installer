0d8b3d81998a80117c6be39e083a0a7aec18aadc
RubenDguez <argenis.dominguez@hotmail.com>
2026-02-17 21:04:49 -0500

    add: playcucumber.config.ts file to download

--- dist/fileMapper.js
+++ dist/fileMapper.js
@@ -1,9 +1,7 @@
 import path from "node:path";
 const baseUrl = new URL("https://raw.githubusercontent.com/RubenDguez/playwright-cucumber-installer/refs/heads/bow/src/template/");
 export const fileUrlDestinationMapper = [
-    {
-        url: new URL(".gitignore", baseUrl).toString(),
-        path: path.join(process.cwd(), ".gitignore"),
-    },
+    { url: new URL(".gitignore", baseUrl).toString(), path: path.join(process.cwd(), ".gitignore") },
+    { url: new URL("playcucumber.config.ts", baseUrl).toString(), path: path.join(process.cwd(), ".gitignore") }
 ];
 //# sourceMappingURL=fileMapper.js.map
--- src/fileMapper.ts
+++ src/fileMapper.ts
@@ -3,8 +3,6 @@ import path from "node:path";
 const baseUrl = new URL("https://raw.githubusercontent.com/RubenDguez/playwright-cucumber-installer/refs/heads/bow/src/template/");
 
 export const fileUrlDestinationMapper: Array<{ url: string, path: string }> = [
-    {
-        url: new URL(".gitignore", baseUrl).toString(),
-        path: path.join(process.cwd(), ".gitignore"),
-    },
+    { url: new URL(".gitignore", baseUrl).toString(), path: path.join(process.cwd(), ".gitignore") },
+    { url: new URL("playcucumber.config.ts", baseUrl).toString(), path: path.join(process.cwd(), ".gitignore") }
 ]
